9ff91073de785f2efb3c6a0acdfcc6e6727b73ef
mintep1-student <mintep1@student.op.ac.nz>
2023-10-24 14:55:14 +1300

    Added code for header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Modal from "$lib/components/Modal.svelte";
+  import Nav from "$lib/components/Nav.svelte";
+  let showModal = false;
+</script>
+
+<header>
+  <div class="container">
+    <div class="logo" />
+    <div class="website-name" />
+    <Nav />
+  </div>
+</header>
+
+<!-- Menu - font icon -->
+<button id="menu-open" on:click={() => (showModal = true)}>
+  <i class="fa-solid fa-bars" />
+  <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512">
+    <path
+      d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 
+                  32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 
+                  32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 
+                  32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+    />
+  </svg>
+</button>
+
+<Modal bind:showModal>
+  <Nav />
+</Modal>
+
+
+<style>
+
+</style>
---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,84 @@
+<nav class="header-nav">
+  <a href="/about" class="link">
+    <div class="btn-text">About</div>
+  </a>
+  <a href="/contact" class="link">
+    <div class="btn-text">Contact</div>
+  </a>
+  <a href="/join" class="link">
+    <div class="btn-text">Join</div>
+  </a>
+  <a href="/events" class="link">
+    <div class="btn-text">Events</div>
+  </a>
+</nav>
+
+<style>
+  .header-nav {
+    column-gap: 0.5em;
+    align-items: center;
+    display: flex;
+    position: static;
+    float: right;
+  }
+
+  .header-nav > .link {
+    border-radius: 100vw;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+    font-weight: 400;
+    line-height: 1.4;
+    text-decoration: none;
+    display: flex;
+    transition: all 0.3s ease-in-out;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+    padding-block: 0.5rem;
+    padding-inline: 1.25rem;
+    background-color: rgba(84, 163, 253, 0.8);
+    justify-content: center;
+    color: #ffff;
+    border: 3px solid #ffffff4d;
+    outline: none;
+    overflow: hidden;
+  }
+
+  .header > .link:hover {
+    transform: scale(1.04);
+    border-color: #fff9;
+  }
+
+  .header > .link:hover::before {
+    animation: shine 1.5s ease-out infinite;
+  }
+
+  .header > .link::before {
+    content: "";
+    position: absolute;
+    width: 100px;
+    height: 100%;
+    background-image: linear-gradient(
+      120deg,
+      rgba(0, 29, 63, 0) 30%,
+      rgba(243, 197, 255, 0.8),
+      rgba(0, 29, 63, 0) 70%
+    );
+    top: 0;
+    left: -100px;
+    opacity: 0.6;
+  }
+
+  @keyframes shine {
+    0% {
+      left: -100px;
+    }
+
+    60% {
+      left: 100%;
+    }
+
+    to {
+      left: 100%;
+    }
+  }
+
+</style>
