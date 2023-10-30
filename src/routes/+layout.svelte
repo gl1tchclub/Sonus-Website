95cda2519c75d8d2c1d837926f3fbd5da2925333
mintep1-student <mintep1@student.op.ac.nz>
2023-10-31 12:02:13 +1300

    Added more grid stuff to layout - not working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,27 +6,23 @@
     import Header from '$lib/components/Header.svelte';
     import SideNav from '../lib/components/SideNav.svelte';
 
-    let screenSize;
-    let width = screenSize - 250;
 </script>
 
 <Header />
 <SideNav />
-<div class="layout-grid" style="width: {width}">
+<div class="layout-grid">
     <slot />
 </div>
-
-<svelte:window bind:innerWidth={screenSize} />
 
 <style>
     .layout-grid {
         display: flex;
-        flex-direction: column;
-        /* top: 0; */
+        top: 0;
         left: 17em;
         height: 150vh;
-        position: absolute;
-        margin: 0 -250px 0 0;
+        max-width: 82.7%;
+        position: relative;
+        margin: 0 250px 0 0;
     }
 
     :global(body) {
