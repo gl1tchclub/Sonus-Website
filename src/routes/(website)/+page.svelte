7433a3d6eb20ade0eb342f41281763fba2c67e8b
mintep1-student <mintep1@student.op.ac.nz>
2023-11-06 17:31:25 +1300

    Added colors to main css, finished html/css for brand name, adding next section code

--- src/routes/(website)/+page.svelte
+++ src/routes/(website)/+page.svelte
@@ -10,11 +10,11 @@
 })
 </script>
 
-<div class="loading">
+<!-- <div class="loading">
     {#each loading_arr as letter, i}
         <span style="--i:{i+1}">{letter}</span>
     {/each}
-</div>
+</div> -->
 
 <div class="home-container">
   <div class="top-section">
@@ -24,6 +24,9 @@
           <h1 class="brand-name">
             <span class="reveal-delay"> Sonus </span>
           </h1>
+          <!-- <h1 class="brand-name outer-text">
+            <span class="reveal-delay"> Sonus </span>
+          </h1> -->
           <img
             src="./img/just-icon.png"
             class="brand-logo"
@@ -34,6 +37,14 @@
         <div class="btn"><JoinButton /></div>
       </div>
       <div class="">lower content</div>
+    </div>
+  </div>
+  <div class="second-section">
+    <div class="middle-container">
+        <h1>DISCOVER</h1>
+        <h1>CREATE</h1>
+        <h1>LISTEN</h1>
+        <h1>REPEAT</h1>
     </div>
   </div>
 </div>
@@ -85,10 +96,33 @@
     position: absolute;
     z-index: 1;
     top: -10px;
-    font-size: 100px;
-    height: 30%;
-    opacity: 0.9;
+    font-size: 150px;
+    height: 60%;
+    width: 100%;
+    opacity: 0.95;
+    color: #f0a6ca;
+    overflow: hidden;
+    text-shadow: 1px 1px 1px #efc3e6,
+        1px 2px 1px #efc3e6,
+        1px 3px 1px #efc3e6,
+        1px 4px 1px #efc3e6,
+        1px 5px 1px #efc3e6,
+        1px 6px 1px #efc3e6,
+        1px 7px 1px #efc3e6,
+        1px 8px 1px #efc3e6,
+        1px 9px 1px #efc3e6,
+        1px 10px 1px #efc3e6,
+    1px 18px 6px rgba(16,16,16,0.4),
+    1px 22px 10px rgba(16,16,16,0.2),
+    1px 25px 35px rgba(16,16,16,0.2),
+    1px 30px 60px rgba(16,16,16,0.4);
   }
+
+  /* .brand-name.outer-text {
+    font-size: 155px;
+    color: #efc3e6;
+    height: 50%;
+  } */
 
   .brand-logo {
     opacity: 0.6;
@@ -97,5 +131,6 @@
   .btn {
     width: 300px;
     display: block;
+    z-index: 1;
   }
 </style>
